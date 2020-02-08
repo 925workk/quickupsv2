e4090b0ec5d886fc80a1bc9ce0e2ba7474138daf
Alex Pham <925workk@gmail.com>
2020-02-08 11:00:10 -0700

    fixed server js.

--- server.js
+++ server.js
@@ -28,7 +28,3 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
