70add300585205d51510e4bf2e1831233f879166
Alex Pham <925workk@gmail.com>
2020-02-08 11:16:26 -0700

    post pickups update.

--- client/src/pages/Postpickups/index.js
+++ client/src/pages/Postpickups/index.js
@@ -2,81 +2,95 @@ import React, { Component } from "react";
 import Navbar from "../../components/NavBar";
 import axios from "axios";
 import Footer from "../../components/Footer";
+const form_template = {
+    storeName: "",
+    pickUpAddress: "",
+    pickUpCity: "",
+    pickUpState: "",
+    pickUpZip: "",
+    pickUpFloor: "",
+    userName: "",
+    dropOffAddress: "",
+    dropOffCity: "",
+    dropOffState: "",
+    dropOffZip: "",
+    dropOffFloor: "",
+    payOut: "",
+    phoneNumber: "",
+    merchandise: ""
+}
 class Postpickups extends Component {
     state = {
-        results: []
+        results: { ...form_template }
     };
-
     handleFormSubmit = event => {
-        let tempData = {
-            storeName: this.state.storeName,
-            pickUpAddress: this.state.pickUpAddress,
-            pickUpCity: this.state.pickUpCity,
-            pickUpState: this.state.pickUpState,
-            pickUpZip: this.state.pickUpZip,
-            pickUpFloor: this.state.pickUpFloor,
-            userName: this.state.userName,
-            dropOffAddress: this.state.dropOffAddress,
-            dropOffCity: this.state.dropOffCity,
-            dropOffState: this.state.dropOffState,
-            dropOffZip: this.state.dropOffZip,
-            dropOffFloor: this.state.dropOffFloor,
-            payOut: this.state.payOut,
-            phoneNumber: this.state.phoneNumber
-        };
         event.preventDefault();
-        console.log(tempData);
-        axios ('/api/postQuickup', {
+        console.log(this.state.results);
+        axios('/api/postQuickup', {
             method: 'POST',
-            data: {
-                tempData
-            }
+            data: { tempData: this.state.results }
         })
             .then(function (response) {
                 return response
             }).then(function (body) {
                 console.log(body);
             });
-            
+        this.setState({
+            results: { ...form_template }
+        })
     };
-
     handleInputChange = event => {
+        const { results } = this.state;
         const name = event.target.name;
         const value = event.target.value;
+        results[name] = value;
         this.setState({
-            [name]: value
+            results
         });
     };
-
-
     render() {
         return (
             <div className="postpickupscontainer">
+                <div className="sweet-loading">
+                </div>
                 <div className="emptydiv"></div>
                 <div className='horizontalline'></div>
                 <Navbar />
                 <form className="formscontainer">
                     <div className="container allforms">
-                    <h1 className="text-center mt-2 mb-4 display-4">POST A PICK UP</h1>
+                        <h1 className="display-4" className="text-center mt-2 mb-4">POST A PICK UP</h1>
                         <div className="form-group">
                             <label for="formGroupExampleInput">Pick up address</label>
-                            <input onChange={this.handleInputChange} type="text" name="storeName" className="form-control" id="storeName" placeholder="Store Name" required/>
+                            <input
+                                onChange={this.handleInputChange}
+                                type="text" name="storeName"
+                                className="form-control"
+                                id="storeName"
+                                placeholder="Store Name"
+                                required value={this.state.results.storeName}
+                            />
                         </div>
                         <div className="form-group">
-                            <input onChange={this.handleInputChange} type="text" name="pickUpAddress" className="form-control" id="pickUpAddress" placeholder="Street Address" required/>
+                            <input
+                                onChange={this.handleInputChange}
+                                type="text" name="pickUpAddress"
+                                className="form-control"
+                                id="pickUpAddress"
+                                placeholder="Street Address"
+                                required value={this.state.results.pickUpAddress} />
                         </div>
                         <div className="form-row">
                             <div className="col">
-                                <input onChange={this.handleInputChange} type="text" name="pickUpCity" className="form-control" id="pickUpCity" placeholder="City" required/>
+                                <input onChange={this.handleInputChange} type="text" name="pickUpCity" className="form-control" id="pickUpCity" placeholder="City" required value={this.state.results.pickUpCity} />
                             </div>
                             <div className="col">
-                                <input onChange={this.handleInputChange} type="text" name="pickUpState" className="form-control" id="pickUpState" placeholder="State" required/>
+                                <input onChange={this.handleInputChange} type="text" name="pickUpState" className="form-control" id="pickUpState" placeholder="State" required value={this.state.results.pickUpState} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="pickUpZip" className="form-control" id="pickUpZip" placeholder="Zip Code" required/>
+                                <input onChange={this.handleInputChange} type="text" name="pickUpZip" className="form-control" id="pickUpZip" placeholder="Zip Code" required value={this.state.results.pickUpZip} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="pickUpFloor" className="form-control" id="pickUpFloor" placeholder="Floor" required/>
+                                <input onChange={this.handleInputChange} type="text" name="pickUpFloor" className="form-control" id="pickUpFloor" placeholder="Floor" required value={this.state.results.pickUpFloor} />
                             </div>
                         </div>
                         <br />
@@ -84,29 +98,29 @@ class Postpickups extends Component {
                         <br />
                         <div className="form-group">
                             <label for="formGroupExampleInput">Drop off address</label>
-                            <input onChange={this.handleInputChange} type="text" name="userName" className="form-control" id="userName" placeholder="Your Name" required/>
+                            <input onChange={this.handleInputChange} type="text" name="userName" className="form-control" id="userName" placeholder="Your Name" required value={this.state.results.userName} />
                         </div>
                         <div className="form-group">
-                            <input onChange={this.handleInputChange} type="text" name="dropOffAddress" className="form-control" id="dropOffAddress" placeholder="Street Address" required/>
+                            <input onChange={this.handleInputChange} type="text" name="dropOffAddress" className="form-control" id="dropOffAddress" placeholder="Street Address" required value={this.state.results.dropOffAddress} />
                         </div>
                         <div className="form-row">
                             <div className="col">
-                                <input onChange={this.handleInputChange} type="text" name="dropOffCity" className="form-control" id="dropOffCity" placeholder="City" required/>
+                                <input onChange={this.handleInputChange} type="text" name="dropOffCity" className="form-control" id="dropOffCity" placeholder="City" required value={this.state.results.dropOffCity} />
                             </div>
                             <div className="col">
-                                <input onChange={this.handleInputChange} type="text" name="dropOffState" className="form-control" id="dropOffState" placeholder="State" required/>
+                                <input onChange={this.handleInputChange} type="text" name="dropOffState" className="form-control" id="dropOffState" placeholder="State" required value={this.state.results.dropOffState} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="dropOffZip" className="form-control" id="dropOffZip" placeholder="Zip Code" required/>
+                                <input onChange={this.handleInputChange} type="text" name="dropOffZip" className="form-control" id="dropOffZip" placeholder="Zip Code" required value={this.state.results.dropOffZip} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="dropOffFloor" className="form-control" id="dropOffFloor" placeholder="Floor" required/>
+                                <input onChange={this.handleInputChange} type="text" name="dropOffFloor" className="form-control" id="dropOffFloor" placeholder="Floor" required value={this.state.results.dropOffFloor} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="payOut" className="form-control" id="payOut" placeholder="Pay Out (Minimum $15)" required/>
+                                <input onChange={this.handleInputChange} type="text" name="payOut" className="form-control" id="payOut" placeholder="Pay Out (Minimum $15)" required value={this.state.results.payOut} />
                             </div>
                             <div className="form-group mx-sm-3 mb-2">
-                                <input onChange={this.handleInputChange} type="text" name="phoneNumber" className="form-control" id="phoneNumber" placeholder="Phone Number" required/>
+                                <input onChange={this.handleInputChange} type="text" name="phoneNumber" className="form-control" id="phoneNumber" placeholder="Phone Number" required value={this.state.results.phoneNumber} />
                             </div>
                         </div>
                         <br />
@@ -114,7 +128,7 @@ class Postpickups extends Component {
                         <br />
                         <div className="form-group">
                             <label for="formGroupExampleInput">List of merchandise</label>
-                            <input onChange={this.handleInputChange} type="text" name="storeName" className="form-control" id="merchandise" placeholder="Refrigerator, Stove, Freezer" required/>
+                            <input onChange={this.handleInputChange} type="text" name="merchandise" className="form-control" id="merchandise" placeholder="Refrigerator, Stove, Freezer" required value={this.state.results.merchandise} />
                         </div>
                         <div className="form-row">
                             <div className="col">
